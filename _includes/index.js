7297f268e854b1e6cd17347f7de0082d9a2c575c
trooper-transfer <studio@trooper-transfer.com>
2026-02-21 10:33:23 +0100

    clamp requested page index.js

--- _includes/index.js
+++ _includes/index.js
@@ -1,6 +1,6 @@
 function change_page(newPage)
 {
-    const requestedIndex = min(max(newPage - 1, 0), {{ last_index }});
+    const requestedIndex = Math.min(Math.max(newPage - 1, 0), {{ last_index }});
     console.log(requestedIndex);
     console.log("{{ curr_page }}");
 }
