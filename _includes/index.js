e5dc1f5475b53c0fc50c9269533772f83464eb52
trooper-transfer <studio@trooper-transfer.com>
2026-02-21 11:01:24 +0100

    udpate index.js

--- _includes/index.js
+++ _includes/index.js
@@ -2,5 +2,5 @@ function change_page(newPage)
 {
     const requestedIndex = Math.min(Math.max(newPage - 1, 0), {{ last_index }});
     const posts = {{ posts | jsonify }};
-    window.Location.href = {{ site.url }} + posts[requestedIndex].url;
+    window.Location.href = "{{ site.url }}" + posts[requestedIndex].url;
 }
