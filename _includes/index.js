b575a39d0858c60ba619dc9511d279e71a1760a2
trooper-transfer <studio@trooper-transfer.com>
2026-02-21 11:59:32 +0100

    update index.js

--- _includes/index.js
+++ _includes/index.js
@@ -4,7 +4,7 @@ const baseUrl = "{{ site.url }}";
 function change_page(newPage)
 {
     const requestedIndex = Math.min(Math.max(newPage - 1, 0), 2);
-    console.log(pages);
+    console.log(pages[requestedIndex]);
     
     //window.Location.href = posts[requestedIndex].url;
 }
