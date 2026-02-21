74d5bed6aeae74964b7854592a1b5a42631df693
trooper-transfer <studio@trooper-transfer.com>
2026-02-21 11:44:18 +0100

    update index.js

--- _includes/index.js
+++ _includes/index.js
@@ -1,4 +1,6 @@
-function change_page(newPage, pages)
+const pages = {{ posts | jsonify }};
+
+function change_page(newPage)
 {
     const requestedIndex = Math.min(Math.max(newPage - 1, 0), 2);
     console.log(pages);
