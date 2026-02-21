f05dcff33b422060e0d6166b352bf076b73cfd1c
trooper-transfer <studio@trooper-transfer.com>
2026-02-21 10:54:30 +0100

    go to requested page index.js

--- _includes/index.js
+++ _includes/index.js
@@ -2,5 +2,5 @@ function change_page(newPage)
 {
     const requestedIndex = Math.min(Math.max(newPage - 1, 0), {{ last_index }});
     const posts = {{ posts | jsonify }};
-    console.log(posts[requestedIndex]);
+    window.Location.href = posts[requestedIndex].url;
 }
